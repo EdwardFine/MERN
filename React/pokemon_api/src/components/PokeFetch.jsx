6586303d9e50d50a7fd965df03718ae99b93ed54
EdwardFine <edfine03@hotmail.com>
2023-06-12 10:39:18 -0700

    Added Axios.get

--- React/pokemon_api/src/components/PokeFetch.jsx
+++ React/pokemon_api/src/components/PokeFetch.jsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import axios from 'axios'
 
 const PokeFetch = () => {
     const [poke,setPoke] = useState([]);
@@ -8,19 +9,30 @@ const PokeFetch = () => {
         .then((response)=>response.json())
         .then((response)=>{
             console.log(response)
+            console.log("Using fetch on the pokeapi")
             setPoke(response.results)
         })
         .catch((error) => console.log(error))
     }
 
+    const axiosPoke = () =>{
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=1010")
+        .then((response)=>{
+            console.log(response)
+            console.log("Using Axios.get on the pokeapi")
+            setPoke(response.data.results)
+        })
+    }
+
   return (
     <div style={{marginTop:'25px'}}>
-        <button onClick={fetchPoke}>Fetch Pokemon</button>
+        <button onClick={fetchPoke}>Fetch Pokemon</button><br />
+        <button onClick={axiosPoke}>Axios Pokemon</button>
         <ul>
         {poke.map((p,i)=>{
             return(
                 <li key={i}>
-                    <p>{p.name[0].toUpperCase() + p.name.slice(1)}</p>
+                    <p>{i+1}. {p.name[0].toUpperCase() + p.name.slice(1)}</p>
                     <hr />
                 </li>
             )
