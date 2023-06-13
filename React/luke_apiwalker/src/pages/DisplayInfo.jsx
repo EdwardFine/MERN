382339cb45c3e3bdd55d27e2da03b305d97ef5c2
EdwardFine <edfine03@hotmail.com>
2023-06-13 11:59:08 -0700

    Editted bad request

--- React/luke_apiwalker/src/pages/DisplayInfo.jsx
+++ React/luke_apiwalker/src/pages/DisplayInfo.jsx
@@ -23,7 +23,7 @@ const DisplayInfo = () => {
     
   return (
     <div>
-        {infoErrors?<h1>Bad Request. Try a different request.</h1>:
+        {infoErrors?<div><h1>These aren't the droids you're looking for</h1> <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2019%2F11%2Frevenge-of-the-sith-2000.jpg&q=60" alt="Obi-wan" style={{width:'500px'}}/></div>:
         catagory==='people'?<DisplayPerson person={info}/>:
         catagory==='planets'?<DisplayPlanet planet={info}/>:
         catagory==='starships'?<DisplayStarShips starship={info}/>:
