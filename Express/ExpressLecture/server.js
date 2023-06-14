34db41e84dea6ab4a81a8e8a7fe54db5a6f93075
EdwardFine <edfine03@hotmail.com>
2023-06-14 14:04:51 -0700

    Added comments

--- Express/ExpressLecture/server.js
+++ Express/ExpressLecture/server.js
@@ -1,9 +1,10 @@
 // import express and store express in a variable called express
 const express = require("express");
 
+//Initialize the express application and store it in a variable called 'app'
 const app = express();
 
-// Give a port the express app will be listening for.
+// Give a port the express app will be listening for, typically 8000.
 const port = 8000;
 // port 21 - ftp
 // port 22 - ssh
@@ -11,9 +12,10 @@ const port = 8000;
 // port 443 - https
 // total ports: 65535
 
-// Allow the application to parse our information via json
+// Allow the application to parse our information via json (form information)
 app.use(express.json())
 
+//Allow the application to accept form information
 app.use(express.urlencoded({ extended: true }))
 
 const menuItems = [,
@@ -38,6 +40,11 @@ const users = [
     { firstName: "Momiji", lastName: "Inubashiri" }
 ];
 
+//get=retrieve all or retrieve one
+//post=create something new
+//put=update something
+//delete=delete something
+
 app.get("/api/users", (req, res) => {
     res.json(users);
 });
@@ -48,6 +55,16 @@ app.get('/api', (request, response) => response.json({ message: "Hola!!" }))
 
 app.get('/api/menu', (req, res) => res.json({ count: menuItems.length, results: menuItems }))
 
+// if we want to get a user with a specific id, we can make the id a part of the url
+// be sure to preface the id variable with a `:` colon
+app.get("/api/users/:id", (req, res) => {
+    // we can get this `id` variable from req.params
+    console.log(req.params.id);
+    // assuming this id is the index of the users array we could return one user this way
+    res.json(users[req.params.id]);
+});
+
+//in postman: form data goes into: Body>x-www-form-urlencoded
 app.post("/api/users", (req, res) => {
     // req.body will contain the form data from Postman or from React
     console.log(req.body);
@@ -58,14 +75,6 @@ app.post("/api/users", (req, res) => {
     res.json({ status: "ok" });
 });
 
-// if we want to get a user with a specific id, we can make the id a part of the url
-// be sure to preface the id variable with a `:` colon
-app.get("/api/users/:id", (req, res) => {
-    // we can get this `id` variable from req.params
-    console.log(req.params.id);
-    // assuming this id is the index of the users array we could return one user this way
-    res.json(users[req.params.id]);
-});
 
 app.patch("/api/users/:id", (req, res) => {
     // we can get this `id` variable from req.params
