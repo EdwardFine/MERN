2d89537ed674514d6a552fdb264ad230d8da3d60
EdwardFine <edfine03@hotmail.com>
2023-06-15 13:17:07 -0700

    Update Mongoose config

--- MongoDB/mongoLecture/server/config/mongoose.config.js
+++ MongoDB/mongoLecture/server/config/mongoose.config.js
@@ -3,6 +3,6 @@ const dbName = process.env.DB;
 const username = process.env.ATLAS_USERNAME;
 const pw = process.env.ATLAS_PASSWORD;
 const uri = `mongodb+srv://${username}:${pw}@cluster0.yrlgqvs.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-mongoose.connect(uri)
+mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log("Established a connection with the database"))
 .catch((err)=>console.log(`Something went wrong with the connection. Error: ${err}`))
